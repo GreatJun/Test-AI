030f9755f77a99c05841a9e255abc0530dd7aca7
GreatJun <ljun970@naver.com>
2023-12-27 19:36:12 +0900

    Simple Character MoveMent

--- Assets/02.Scripts/Player.cs
+++ Assets/02.Scripts/Player.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float speed = 5;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        CharacterMovement();
+    }
+
+    void CharacterMovement()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        transform.position += (new Vector3(x, y)).normalized * Time.deltaTime * speed;
     }
 }
