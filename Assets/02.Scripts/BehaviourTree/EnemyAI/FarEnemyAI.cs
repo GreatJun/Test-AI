673f7e9127e4804215f2359aa89aaa9dd3084320
GreatJun <ljun970@naver.com>
2023-12-29 04:58:21 +0900

    Fix : FarEnemy BT Fix &  Script Remove

--- Assets/02.Scripts/BehaviourTree/EnemyAI/FarEnemyAI.cs
+++ Assets/02.Scripts/BehaviourTree/EnemyAI/FarEnemyAI.cs
@@ -137,8 +137,10 @@ public class FarEnemyAI : MonoBehaviour
         {
             if (Vector3.SqrMagnitude(_detectedPlayer.position - transform.position) < (_attackDistance * _attackDistance))
             {
-                return INode.ENodeState.ENS_Success;
+                //transform.position = Vector3.MoveTowards(transform.position, -_detectedPlayer.position, Time.deltaTime * _movementSpeed);
+                return INode.ENodeState.ENS_Running;
             }
+            // 플레이어 위치 + 사정거리 포지션으로 이동해야함 (플레이어 위치로 이동하는게 아니라)
 
             transform.position = Vector3.MoveTowards(transform.position, _detectedPlayer.position, Time.deltaTime * _movementSpeed);
 
